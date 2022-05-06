5e57d5d30c0861bbe6da75c5db927772e3ed207e
vesper85 <utkarshdeshmukh98@gmail.com>
2022-05-07 00:17:02 +0530

    more updates

--- components/BlogHeader.js
+++ components/BlogHeader.js
@@ -1,4 +1,6 @@
 import blogStyles from '../styles/Blog.module.css'
+import Highlight from './blog-utils/Highlight'
+
 
 
 function BlogHeader() {
@@ -11,7 +13,8 @@ function BlogHeader() {
             <p className="text-[1.05rem] leading-5">
             I am a Web Developer in Pune, Maharashtra. I have a strong interest in development and engineering.
           I enjoy coding products, creating, writing, and reading.
-          Find out more about me!!
+          <Highlight>Find out more about me!!</Highlight>
+          
             </p>
         </div>
     </section>
