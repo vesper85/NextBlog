57ec4abaaa4cd18b1a4a262d6f016348443b424e
vesper85 <utkarshdeshmukh98@gmail.com>
2022-05-06 15:43:24 +0530

    working on aboutme page

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -5,17 +5,17 @@ import styles from '../styles/Home.module.css'
 export default function aboutme(){
   return (
     <main className="h-screen flex flex-col items-center sm:px-4 md:px-[2rem] dark:text-white mt-20">
-        <div className="flex justify-between w-3/5 ">
+        <div className="flex flex-col-reverse sm:flex-row justify-between md:w-3/5 text-[1rem] ">
             {/* flex-1 grow */}
-            <div className="w-3/4 self-center">
+            <div className="flex-1 grow self-center mt-10 sm:mt-0">
                 I&apos;m a Web Developer based in pune, Maharashtra. 
                 I&apos;ve a deep passion for developmentand engineering. 
                 I enjoy building products with code, designing, writing, and reading.
              </div>
              {/* flex-1 */}
-            <div className=" w-2/5"> 
+            <div className=" flex-1"> 
             {/* height and widht with response */}
-                <Image src={profileImg} className={styles.test} quality={100} alt="my profile image" layout="responsive" /> 
+                <Image src={profileImg} quality={100} className={styles.test}  alt="my profile image" layout="responsive" priority /> 
             </div>
         </div>
         <div className="my-12">
